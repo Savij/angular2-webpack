0fcbe57bd1e9db60dea7743c85fbf8b504ac93f6
Jeff <jnoblemcsd@hotmail.com>
2016-08-15 17:21:10 -0400

    fixed broken child route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,10 @@ export const routes: RouterConfig = [
   { path: '', redirectTo: 'site-layout', pathMatch: 'full' },
   { path: 'site-layout', component: SiteLayoutComponent,
     children: [
-      { path:'', component: HomeComponent }
+      { path:'', component: HomeComponent },
+      { path: 'about', component: AboutComponent}
       ]},
-  { path: 'about', component: AboutComponent}
+  
 ];
 
 export const APP_ROUTER_PROVIDERS = [
